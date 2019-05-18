fc53652e83d3491dfd2c5210635825b43c6da559
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-05-18 16:48:46 +0200

    Wykres

--- main.m
+++ main.m
@@ -14,10 +14,25 @@ clear;
 dydx = @(x, y) (-2*pi*exp(-x)*sin(2*pi*x) - y);
 h = 0.001;
 
-w = metodaEulera(dydx, h, 0, 1, 10);
-w = metodaHeuna(dydx, h, 0, 1, 10);
+wynikEulera = metodaEulera(dydx, h, 0, 1, 10);
+wynikHeuna = metodaHeuna(dydx, h, 0, 1, 10);
 
-plot(w(1, :), w(2, :))
+% Wynik analityczny
+f = @(x) (exp(-x)*cos(2*pi*x));
+x = 0:h:10;
+y = arrayfun(f, x);
+
+plot(x, y, '-', 'DisplayName','Wynik analityczny');
+title("y = e\^(-x)*cos(2*pi*x) oraz wyniki rozwiazan metodami numerycznymi");
+xlabel("x");
+ylabel("y");
+hold on;
+grid on;
+
+plot(wynikEulera(1, :), wynikEulera(2, :), 'o', 'DisplayName','Rozwiazania metoda Eulera');
+plot(wynikHeuna(1, :), wynikHeuna(2, :), 'x', 'DisplayName','Rozwiazania metoda Heuena');
+
+legend();
 
 %%
 h = 2;
